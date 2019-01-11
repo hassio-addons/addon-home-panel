39ca305850ca9fca2135d31bb0c0d7462aec6ddf
Franck Nijhof <frenck@addons.community>
2019-01-11 22:13:11 +0100

    :shirt: Codestyling improvements

--- home-panel/rootfs/etc/cont-init.d/20-config.sh
+++ home-panel/rootfs/etc/cont-init.d/20-config.sh
@@ -6,10 +6,12 @@
 # shellcheck disable=SC1091
 source /usr/lib/hassio-addons/base.sh
 
+declare config_file
+
 config_file="/config/$(hass.config.get 'config_file')"
 
-if [ ! -f "$config_file" ]; then
+if ! hass.file_exists "${config_file}"; then
   hass.log.info "Config file does not exist. Creating.."
-  cp /etc/home-panel/home-panel-config.default.json "$config_file"
-  hass.log.info "Created. You should now edit this file at '$config_file'"
+  cp /etc/home-panel/home-panel-config.default.json "${config_file}"
+  hass.log.info "Created. You should now edit this file at '${config_file}'"
 fi
