b78ed7750ade2e9cf3ad47dee9f38b74fdccc0ea
Timmo <contact@timmo.xyz>
2019-03-30 11:31:16 +0000

    :hammer: Move secret to init script

---
+++ home-panel/rootfs/etc/cont-init.d/20-secrets.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Community Hass.io Add-ons: Home Panel
+# This updates the internal auth secret for the API
+# ==============================================================================
+# shellcheck disable=2094,2016
+bashio::jq /opt/api/config/default.json '.authentication.secret="$(openssl rand -base64 32)"' > /opt/api/config/default.json
