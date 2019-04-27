b68d5d923444148277742a24b18c53591aecffbf
Timmo <contact@timmo.xyz>
2019-04-28 00:33:58 +0100

    :hammer: :sparkles: Rewrite nginx and add ingress support

--- home-panel/rootfs/etc/cont-init.d/10-requirements.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/with-contenv bashio
-# ==============================================================================
-# Community Hass.io Add-ons: Home Panel
-# This checks if all user configuration requirements are met
-# ==============================================================================
-# Check SSL settings
-bashio::config.require.ssl
