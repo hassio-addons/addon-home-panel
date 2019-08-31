d0991fcf8a720749ae9f8ecf174aa78dc65ce0b8
Timmo <contact@timmo.xyz>
2019-08-31 22:42:52 +0100

    :arrow_up: Updates

--- home-panel/rootfs/etc/cont-init.d/secrets.sh
+++ home-panel/rootfs/etc/cont-init.d/secrets.sh
@@ -4,5 +4,4 @@
 # This updates the internal auth secret for the API
 # ==============================================================================
 # shellcheck disable=2094,2016
-# TODO: Fix this
-# bashio::jq /opt/panel/config/default.json '.authentication.secret="$(openssl rand -base64 32)"' > /opt/panel/config/default.json
+bashio::jq /opt/panel/config/default.json '.authentication.secret="$(openssl rand -base64 32)"' > /opt/panel/config/default.json
