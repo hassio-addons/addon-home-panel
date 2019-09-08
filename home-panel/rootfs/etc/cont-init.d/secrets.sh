1aef54deda47a2e6ddaf8ac452fcbae35cd44b0e
Timmo <contact@timmo.xyz>
2019-09-08 19:02:51 +0100

    :shirt: Shellcheck fix

--- home-panel/rootfs/etc/cont-init.d/secrets.sh
+++ home-panel/rootfs/etc/cont-init.d/secrets.sh
@@ -3,7 +3,7 @@
 # Community Hass.io Add-ons: Home Panel
 # This updates the internal auth secret for the API
 # ==============================================================================
-if [ $(cat /opt/panel/config/default.json | grep -c "API_AUTH_SECRET") = 0 ]; then
+if [ "$(cat /opt/panel/config/default.json | grep -c 'API_AUTH_SECRET')" = 0 ]; then
     bashio::log.info "Update auth secret.."
     sed -i -e "s/API_AUTH_SECRET/$(openssl rand -base64 32)/g" /opt/panel/config/default.json
 fi
