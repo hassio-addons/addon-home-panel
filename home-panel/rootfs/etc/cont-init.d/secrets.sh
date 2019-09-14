44ff20cc1af6f1ca2288b6695adecec5c36d1968
Timmo <contact@timmo.xyz>
2019-09-14 23:15:31 +0100

    :hammer:  Persist secret and db

--- home-panel/rootfs/etc/cont-init.d/secrets.sh
+++ home-panel/rootfs/etc/cont-init.d/secrets.sh
@@ -3,10 +3,18 @@
 # Community Hass.io Add-ons: Home Panel
 # This updates the internal auth secret for the API
 # ==============================================================================
-if [ "$(grep -i 'API_AUTH_SECRET' /opt/panel/config/default.json)" = 0 ]; then
-    bashio::log.info "Update auth secret.."
-    sed -i -e "s/API_AUTH_SECRET/$(openssl rand -base64 32)/g" /opt/panel/config/default.json
+declare key
+
+if ! bashio::fs.file_exists "/data/secret.txt"; then
+    bashio::log.info "Generating secret"
+    newkey=$(openssl rand -base64 32)
+    echo "${newkey}" > /data/secret.txt
 fi
 
-# Force database to use /data
+key=$(cat /data/secret.txt)
+
+# Set secret to persistent secret file
+sed -i "s/API_AUTH_SECRET/${key}/g" /opt/panel/config/default.json
+
+# Set database to /data
 sed -i "s#../db#/data#g" /opt/panel/config/default.json
