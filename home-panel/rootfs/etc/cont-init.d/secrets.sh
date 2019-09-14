fd47050d0e3d74b811b7994c694137bc1ac82763
Timmo <contact@timmo.xyz>
2019-09-14 22:56:51 +0100

    :ambulance: Remove bad grep check

--- home-panel/rootfs/etc/cont-init.d/secrets.sh
+++ home-panel/rootfs/etc/cont-init.d/secrets.sh
@@ -8,7 +8,5 @@ if [ "$(grep -i 'API_AUTH_SECRET' /opt/panel/config/default.json)" = 0 ]; then
     sed -i -e "s/API_AUTH_SECRET/$(openssl rand -base64 32)/g" /opt/panel/config/default.json
 fi
 
-if [ "$(grep -i '../db' /opt/panel/config/default.json)" = 0 ]; then
-    # Force database to use /data
-    sed -i "s#../db#/data#g" /opt/panel/config/default.json
-fi
+# Force database to use /data
+sed -i "s#../db#/data#g" /opt/panel/config/default.json
