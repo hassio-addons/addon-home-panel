98623b4d781f01c9c84e5f07407b8be6b9107c1d
Timmo <contact@timmo.xyz>
2021-02-19 22:19:38 +0000

    Update and fix paths

--- home-panel/rootfs/etc/cont-init.d/secrets.sh
+++ home-panel/rootfs/etc/cont-init.d/secrets.sh
@@ -15,8 +15,8 @@ key=$(cat /data/secret.txt)
 
 # Set secret to persistent secret file
 bashio::log.info "Update secret in config"
-sed -i "s#API_AUTH_SECRET#${key}#g" /opt/panel/output/config/default.json
+sed -i "s#API_AUTH_SECRET#${key}#g" /opt/panel/config/default.json
 
 # Set database to /data
 bashio::log.info "Update database path in config"
-sed -i "s#../db#/data#g" /opt/panel/output/config/default.json
+sed -i "s#../db#/data#g" /opt/panel/config/default.json
